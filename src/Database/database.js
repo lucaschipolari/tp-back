192446175329fa7faae6d602154667210856af9d
lucas <zlucaschipolariz@gmail.com>
2024-07-26 19:59:44 -0300

    ordene la carpetas principales

---
+++ src/Database/database.js
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const { MONGODB } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(MONGODB)
+  .then(() => {
+    console.log("Connected to Mongo");
+  })
+  .catch(() => {
+    console.log("Failed to connect to Mongo");
+  });
